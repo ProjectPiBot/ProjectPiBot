2dbebff84a92f63a5c42609d83f44d0fd4fb8746
thfzk <105499768+thfzk@users.noreply.github.com>
2023-05-31 17:05:59 +0900

    Update test_main_db_date.py

--- test_main_db_date.py
+++ test_main_db_date.py
@@ -16,7 +16,7 @@ tomorrow = tm.strftime("%Y년 %m월 %d일")                                    #
 day_after_tomorrow = dat.strftime("%Y년 %m월 %d일")                             #모레 날짜를 문자열로 변경
 
 location_city = ['서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기도', '강원도', '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도', '제주도']
-commands = ["일정", "날씨" "확인"]                                             # api를 호출해야하는 명령 목록
+commands = ["일정", "날씨", "확인"]                                             # api를 호출해야하는 명령 목록
 
 similar = ["하이본", "파이봇", "사이봇", "타이머"]
 
@@ -63,12 +63,14 @@ while True:
         for current in commands:                            # 특정 명령 단어가
             if current in data:                             # 말에 있는지 확인
                 command = current                           # 특정 명령이 말에 들어가 있을 경우 해당 명령을 command 변수에 저장
+                print("command? : ", command)
 
         if command != "":
             if command == "확인":
                 TTS.speak("언제 일정을 확인 할까요?.")
+                data = stt()
 
-                if "오늘" in date_data:
+                if "오늘" in data:
                     TTS.speak("오늘 일정을 확인합니다.")
                     date = today
                     con = pymysql.connect(host='localhost', user='root', password='1234',
@@ -76,13 +78,14 @@ while True:
                     cur = con.cursor()
                     sql = "select * from schedule where date='" + date +"'"
                     cur.execute(sql)
-
+                    print("date? : ", data)
+                    print("sql? : ", sql)
                     # 데이타 Fetch
                     rows = cur.fetchall()
                     print(rows)
                     con.close()
 
-                    TTS.speak(rows)
+                    TTS.speak(str(rows))
                     
 
             if command == "일정":                                # "일정"이 입력 되면 실행
