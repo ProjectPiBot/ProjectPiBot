624002ff0eb9aaf5374416c0aae9fe86e026259e
back1534 <128139834+back1534@users.noreply.github.com>
2023-06-08 12:50:29 +0900

    Update current_weather.py

--- current_weather.py
+++ current_weather.py
@@ -16,10 +16,10 @@ def get_current_weather():
     res = json.loads(res.text)
     
     
-    #name = res['name']                         # 위치
+    name = res['name']                          # 위치
     temp = res['main']['temp']                  # 온도
     weather = res['weather'][0]['description']  # 날씨
     
-    response = f"현재 위치의 날씨는 {weather}이고 온도는 {temp}도 입니다."
+    response = f"현재 위치는 {name} 날씨는 {weather}이고 온도는 {temp}도 입니다."
     
     return response
